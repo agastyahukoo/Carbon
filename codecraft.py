a5157bcfc2f8d80d0c59c2b6ad69c0022f57664b
Agastya Hukoo <136306593+agastya-hukoo@users.noreply.github.com>
2023-07-20 17:34:38 +0530

    Add files via upload

---
+++ codecraft.py
@@ -0,0 +1,6 @@
+def main():
+    text = """Welcome to CodeCraft, the HTML editor designed to streamline your web development workflow. Download CodeCraft today and unlock your web development potential like never before. Start crafting beautiful, dynamic, and responsive websites with ease. Happy coding! Official Website: https://agastya-hukoo.github.io/Code-Craft/"""
+    print(text)
+
+if __name__ == "__main__":
+    main()
